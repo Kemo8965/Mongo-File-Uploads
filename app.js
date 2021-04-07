c0ddb1a82212fd496162edebedc77b5211be8f02
Kemo8965 <78011284+Kemo8965@users.noreply.github.com>
2021-04-07 22:33:39 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,160 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const { json } = require('body-parser');
+const app = express();
+
+
+//middleware
+
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+
+//Mongo URI
+const mongoURI = 'mongodb+srv://dbUser:dbUser8965@cluster0.7ehrv.mongodb.net/FileUploads_DB';
+// const mongoURI = 'mongodb://localhost:27017/FileUploads_DB';
+
+//Mongo Connection
+const conn = mongoose.createConnection(mongoURI);
+
+
+
+
+//GFS Init
+let gfs;
+conn.once('open', () => {
+    gfs = Grid(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+});
+
+//Create a Storage Engine
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'uploads'
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+const upload = multer({ storage });
+
+//@Route GET,
+//@desc Loads Form
+app.get('/', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+        if (!files || files.length === 0) {
+            res.render('index', { files: false });
+
+        } else {
+            files.map(file => {
+                if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+                    file.isImage = true;
+                } else {
+                    file.isImage = false;
+                }
+            });
+            res.render('index', { files: files });
+        }
+    })
+    res.render('index');
+});
+
+//@route GET/files
+//@desc Display all files in JSON
+app.get('/files', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+        //CHECK IF FILES EXIST
+        if (!files || files.length == 0) {
+            return res.status(404).json({
+                err: 'No files exist!'
+            });
+        }
+
+        //FILES EXIST
+        return res.json(files);
+
+    });
+});
+
+//@route GET/file
+//@desc Display single file in JSON
+app.get('/files/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+        //CHECK IF FILES EXIST
+        if (!file || file.length == 0) {
+            return res.status(404).json({
+                err: 'No file exists !'
+            });
+        }
+
+        //FILES EXIST
+        return res.json(file);
+    });
+});
+
+//@route GET/image
+//@desc Display single image
+app.get('/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+        //CHECK IF FILES EXIST
+        if (!file || file.length == 0) {
+            return res.status(404).json({
+                err: 'No file exists !'
+            });
+        }
+
+        //CHECK IF IMAGE EXISTs
+        if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+            //READ OUTPUT TO BROWSER
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.pipe(res);
+        } else {
+            res.status(404).json({
+                err: 'Not an Image!'
+            });
+        }
+    });
+});
+
+//@route POST,
+//desc Uploads file to Database
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.status(200).json({
+        file: `${process.env.BASE_URL}${req.file.filename}`,
+        Message: 'Successfully Uploaded File!',
+        status: 'Success'
+
+    });
+
+    res.status(200).json({
+        file: req.file
+
+    });
+    //json({ file: req.file });
+    console.log(res.file);
+});
+
+
+const port = 5009;
+
+app.listen(port, () =>
+    console.log(`Server listening on port ${port}`));
